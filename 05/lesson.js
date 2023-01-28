8ce5a4a57d8cde6b859697c6a041108f50098f3e
Aleksandr <Aleksandr1@iMac.local>
2023-01-28 16:19:46 +0300

    New file with JS operators

---
+++ 05/lesson.js
@@ -0,0 +1,19 @@
+let string = '18';
+let boolean = true;
+let number = 2;
+
+string + boolean;
+string + number;
+number + boolean;
+
+string * boolean;
+string * number;
+number * boolean;
+
+string / boolean;
+string / number;
+number / boolean;
+
+number = String(number);
+string = Number(string);
+boolean = String(boolean);
